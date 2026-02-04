e9b5f977cd0543d2452c137a2fd4aad74dc9afcc
iglibic <ivanglibic77@gmail.com>
2026-02-04 17:03:31 +0100

    Add files via upload

---
+++ Vjezba6/script.js
@@ -0,0 +1,50 @@
+/**
+ * 1. Create a class called Book will include properties title, author, and year.
+ * It has to have a class method called getDetails that returns
+ * a sentence (string) that describes the book (what is the title, who is the author and what year was it published).
+ * To create that string use string literals (not concatenation).
+ * Create an instance of that class for a book called "The Fellowship of the Ring" by J.R.R. Tolkien that was published 1954
+ * Log the result of calling the getDetails method on that instance
+ */
+
+/**
+ * 2. Create a class EBook by extending the class Book.
+ * The new class should have additional properties downloadLink and fileFormat
+ * It should also have a method called "download" which prints the following sentence:
+ * Downloading 'title' in 'fileFormat' format. Link: 'downloadLink'
+ * Where the values in parentheses are switched with the true values of the class properties using string literals.
+ * Create an instance of the EBook class for a book called 'The Two Towers' by J.R.R. Tolkien that was published in 1954 in mp3 format
+ * and the download link is https://archive.org/details/the-two-towers_soundscape-by-phil-dragash
+ * Log the result of calling the download method on that instance
+ */
+
+/**
+ * 3. Create an object with 3 properties:
+ * 			1. x that is set to 5
+ * 			2. y that is set to 3
+ * 			3. sumXY that is a function that sums the first two object properties
+ * Log to console the result of calling sumXY
+ */
+
+/**
+ * 4. Create an arrow function that takes an email string and returns everything before the @ symbol as the username.
+ * If there’s no @ in the email, return null.
+ * Call the function with a valid and an invalid mail and log the result.
+ */
+
+/*
+ * 5. Create a function that receives 2 parameters: text and word
+ * It counts the number of times a word appears in the text, case insensitive
+ * If the word appears only once the string should say "The word ??? appears 1 time in the following text: ???".
+ * Otherwise it should say "The word ??? appears ??? times in the following text: ???".
+ * The ??? should be replaced with the respective values using string literals without concatenation.
+ */
+
+/**
+ * 6. Write a function that finds and returns the first character in a string that does not repeat.
+ * If all characters repeat, return null.
+ */
+
+/**
+ * 7.Write a function that takes a snake_case string and converts it to camelCase.
+ */
